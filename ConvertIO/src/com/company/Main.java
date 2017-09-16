dae0958429a6f2595990576e5039cafac08399a5
alexander126 <akalapsazov@yahoo.com>
2017-09-16 20:13:02 +0100

    ConvertIO

--- ConvertIO/src/com/company/Main.java
+++ ConvertIO/src/com/company/Main.java
@@ -1,20 +1,22 @@
 package com.company;
 
 import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.Period;
 import java.util.Date;
+import java.util.Scanner;
 import java.util.TimeZone;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        long seconds = 4100;
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int seconds = reader.nextInt();
+        reader.close();
         Date date = new Date(seconds * 1000L);
-        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss"); //
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
         format.setTimeZone(TimeZone.getTimeZone("GMT"));
         String time = format.format(date);
-        System.out.println(seconds + " seconds equals:"+ time +" in hours.");
+        System.out.println(seconds +" equals: " + time +" in hours.");
     }
 }
